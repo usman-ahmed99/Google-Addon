c8492ad7aec397de521e601eefcc57d82cbb5d7a
usman-ahmed99 <usman.ahmed99@hotmail.com>
2025-01-30 15:09:16 +0500

    chore: update addon

--- src/add-on/add-on.controller.ts
+++ src/add-on/add-on.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, Req } from '@nestjs/common';
+import { google } from 'googleapis';
+import asyncHandler from 'express-async-handler';
+import { OAuth2Client } from 'google-auth-library';
+import { gmail } from 'googleapis/build/src/apis/gmail';
 
 @Controller('addon')
 export class AddOnController {
   @Post('render')
   @HttpCode(200)
-  renderHomepage(@Body() body: any) {
+  async renderHomepage(@Body() body: any, @Req() req: any) {
     console.log('endpoint called');
     console.log('body:', JSON.stringify(body, null, 2));
+    const currentMessageId = req.body.gmail.messageId;
+    console.log('currentMessageId:', currentMessageId);
+    const event = req.body;
+    console.log('event:', JSON.stringify(event, null, 2));
+    const accessToken = event.authorizationEventObject.userOAuthToken;
+    console.log('accessToken:', accessToken);
+    const messageToken = event.gmail.accessToken;
+    console.log('messageToken:', messageToken);
+    const auth = new OAuth2Client();
+    auth.setCredentials({ access_token: accessToken });
+
+    const gmailResponse = await gmail['users'].messages.get({
+      id: currentMessageId,
+      userId: 'me',
+      format: 'metadata',
+      auth,
+      headers: { 'X-Goog-Gmail-Access-Token': messageToken },
+    });
+
+    console.log('gmailResponse:', JSON.stringify(gmailResponse, null, 2));
     return {
       action: {
         navigations: [
